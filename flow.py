114b54086fe1c02d97bbbc2838359fa8b7403841
Andres Vazquez <andres@data99.com.ar>
2019-07-03 10:01:23 -0300

    getting API data for comparing

--- flow.py
+++ flow.py
@@ -1,30 +1,66 @@
+"""
+Get updated list of resources from a data.json file
+Get the actual list of resources in data.gov
+Compare both and upgrade data.gov 
+
+Using:
+ - DataFlows: https://github.com/datahq/dataflows/blob/master/TUTORIAL.md
+ - DataPackages: https://frictionlessdata.io/data-packages/
+"""
+
 from dataflows import Flow, printer, dump_to_path, checkpoint
-from functions import get_data_json_from_url, clean_duplicated_identifiers, list_parents_and_childs
+from functions import (get_data_json_from_url, clean_duplicated_identifiers,
+                        list_parents_and_childs, get_actual_ckan_resources_from_api)
 import json
 
-resources_path = 'list-resources.json'  # High priority data.json file
+resources_path = 'list-resources.json'  # High priority data.json file. 
+#TODO get full list from https://catalog.data.gov/api/3/action/package_search?q=%28type:harvest%29&rows=1000
 resources_file = open(resources_path, 'r')
 json_resources = json.load(resources_file)
 
 # try the first one
-name = json_resources[0]['name']
-url = json_resources[0]['url']
+data_json_to_process = json_resources[0]
+name = data_json_to_process['name']  # Nice name of the source
+url = data_json_to_process['url']  # data.json final URL
+source_identifier = data_json_to_process['identifier']  # harves source id at CKAN
 
 Flow(
     get_data_json_from_url(url),
     clean_duplicated_identifiers,
-    dump_to_path(f'datapackages/package_{name}'),
-    checkpoint('data-json-cleaned-duplicates'),
+
+    dump_to_path(f'datapackages/{name}/datajson'),
+    # checkpoint('data-json-cleaned-duplicates'),
+    # printer(num_rows=1), # , tablefmt='html')
+    
+).process()[1]
+
+
+Flow(
+    get_actual_ckan_resources_from_api(harvest_source_id=source_identifier),
+    
+    dump_to_path(f'datapackages/{name}/dataapi'),
     # printer(num_rows=1), # , tablefmt='html')
     
 ).process()[1]
 
 """
-# learn how to https://github.com/datahq/dataflows/blob/master/TUTORIAL.md
-
-# continue working
+# merge both
 Flow(
-    checkpoint('data-json-cleaned-duplicates'),
-    list_parents_and_childs,
+    
+    # define parents and childs identifiers https://github.com/GSA/ckanext-datajson/blob/datagov/ckanext/datajson/harvester_base.py#L145
+    # define catalog_extras https://github.com/GSA/ckanext-datajson/blob/datagov/ckanext/datajson/harvester_base.py#L153
+
+    # Iterate ALL Harvest Objects at CKAN instance https://github.com/GSA/ckanext-datajson/blob/datagov/ckanext/datajson/harvester_base.py#L164
+        # and list which still exist https://github.com/GSA/ckanext-datajson/blob/datagov/ckanext/datajson/harvester_base.py#L174
+        # create a separate list for which are parents
+    
+    # detect which was demoted and which was promoted to parent
+    # https://github.com/GSA/ckanext-datajson/blob/datagov/ckanext/datajson/harvester_base.py#L179
+    # error if one opackage is parent and child (identifier is part of both lists) https://github.com/GSA/ckanext-datajson/blob/datagov/ckanext/datajson/harvester_base.py#L198
+
+    # detect new parents (this one who are parents in data.json but not before) https://github.com/GSA/ckanext-datajson/blob/datagov/ckanext/datajson/harvester_base.py#L202
+
+    # delete all no longer in the remote catalog https://github.com/GSA/ckanext-datajson/blob/datagov/ckanext/datajson/harvester_base.py#L322
+
 ).process()[1]
 """
--- functions.py
+++ functions.py
@@ -1,6 +1,8 @@
 from urllib.request import urlopen
 import json
 from libs.data_json import JSONSchema, DataJSON
+from libs.data_gov_api import CKANPortalAPI
+
 import logging
 logger = logging.getLogger(__name__)
 
@@ -81,3 +83,23 @@ def clean_duplicated_identifiers(rows):
             duplicates.append(row['identifier'])
             logger.error('Duplicated {}'.format(row['identifier']))
     logger.info('{} duplicates deleted. {} OK'.format(len(duplicates), processed))
+
+
+
+def get_actual_ckan_resources_from_api(harvest_source_id=None):
+    logger.info('Extracting from harvest source id: {}'.format(harvest_source_id))
+    cpa = CKANPortalAPI()
+    resources = 0
+    
+    page = 0
+    for packages in cpa.search_packages(harvest_source_id=harvest_source_id):
+        # getting resources in pages of packages
+        page += 1
+        logger.info('PAGE {} from harvest source id: {}'.format(page, harvest_source_id))
+        for package in packages:
+            pkg_resources = len(package['resources'])
+            resources += pkg_resources
+            yield(package)
+
+        logger.info('{} total resources'.format(resources))
+        
