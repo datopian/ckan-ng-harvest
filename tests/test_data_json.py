2681c829d75314cbc1a450a83c1881ddaf99a440
Andres Vazquez <andres@data99.com.ar>
2019-08-05 23:36:39 -0300

    add test for colections

--- tests/test_data_json.py
+++ tests/test_data_json.py
@@ -1,9 +1,93 @@
 import unittest
 from libs.data_json import DataJSON
-base_url = 'https://avdata99.gitlab.io/andres-harvesting-experiments-v2'
+base_url = 'https://datopian.gitlab.io/ckan-ng-harvest'
 
 
 class DataJSONTestClass(unittest.TestCase):
+
+    test_original_datajson_datasets = {
+            "@type": "dcat:Catalog",
+            "describedBy": "https://project-open-data.cio.gov/v1.1/schema/catalog.json",
+            "conformsTo": "https://project-open-data.cio.gov/v1.1/schema",
+            "@context": "https://project-open-data.cio.gov/v1.1/schema/catalog.jsonld",
+            "dataset": [
+                {
+                "identifier": "USDA-26522",
+                "accessLevel": "public",
+                "isPartOf": 'USDA-26521',
+                "contactPoint": {
+                    "hasEmail": "mailto:dataset2@usda.gov",
+                    "@type": "vcard:Contact",
+                    "fn": "Dataset Two"
+                    },
+                "programCode": ["005:044"],
+                "description": "Some notes dataset 2 ...",
+                "title": "Dataset 2 Title",
+                "distribution": [
+                    {
+                    "@type": "dcat:Distribution",
+                    "downloadURL": "http://dataset2.usda.gov/",
+                    "mediaType": "text/html",
+                    "title": "Web Page Datset 2"
+                    },
+                    {
+                    "@type": "dcat:Distribution",
+                    "downloadURL": "http://dataset2.usda.gov/costsavings.json",
+                    "describedBy": "https://management.cio.gov/schemaexamples/costSavingsAvoidanceSchema.json",
+                    "mediaType": "application/json",
+                    "conformsTo": "https://management.cio.gov/schema/",
+                    "describedByType": "application/json"
+                    }
+                ],
+                "license": "https://creativecommons.org/licenses/by/4.0",
+                "bureauCode": ["005:41"],
+                "modified": "2018-12-23",
+                "publisher": {
+                    "@type": "org:Organization",
+                    "name": "Agricultural Marketing Service"
+                    },
+                "keyword": ["Datset2", "wholesale market"]
+                },
+
+                {
+                "identifier": "USDA-26521",
+                "accessLevel": "public",
+                "contactPoint": {
+                    "hasEmail": "mailto:Fred.Teensma@ams.usda.gov",
+                    "@type": "vcard:Contact",
+                    "fn": "Fred Teensma"
+                    },
+                "programCode": ["005:047"],
+                "description": "Some notes ...",
+                "title": "Fruit and Vegetable Market News Search",
+                "distribution": [
+                    {
+                    "@type": "dcat:Distribution",
+                    "downloadURL": "http://marketnews.usda.gov/",
+                    "mediaType": "text/html",
+                    "title": "Web Page"
+                    },
+                    {
+                    "@type": "dcat:Distribution",
+                    "downloadURL": "http://www.usda.gov/digitalstrategy/costsavings.json",
+                    "describedBy": "https://management.cio.gov/schemaexamples/costSavingsAvoidanceSchema.json",
+                    "mediaType": "application/json",
+                    "conformsTo": "https://management.cio.gov/schema/",
+                    "describedByType": "application/json"
+                    }
+                ],
+                "license": "https://creativecommons.org/licenses/by/4.0",
+                "bureauCode": ["005:45"],
+                "modified": "2014-12-23",
+                "publisher": {
+                    "@type": "org:Organization",
+                    "name": "Agricultural Marketing Service"
+                    },
+                "keyword": ["FOB", "wholesale market"],
+
+                }
+                ]
+            }
 
     def test_load_from_url(self):
         dj = DataJSON()
@@ -66,3 +150,17 @@ class DataJSONTestClass(unittest.TestCase):
 
         self.assertFalse(ret)  # schema works but has errors
         self.assertEqual(1, len(errors))  # 1 schema errors
+
+    def test_load_from_data_json_object(self):
+        # test loading a data.json dict
+        dj = DataJSON()
+        dj.read_dict_data_json(data_json_dict=self.test_original_datajson_datasets)
+        ret, error = dj.validate_json()
+        print(error)
+
+        for dataset in dj.datasets:
+            if dataset['identifier'] == 'USDA-26521':
+                assert dataset['is_collection'] == True
+            if dataset['identifier'] == 'USDA-26522':
+                assert dataset['collection_pkg_id'] == ''
+
