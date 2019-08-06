d477a08068f1bf24e0773dc11d0d445de7fdaba8
Andres Vazquez <andres@data99.com.ar>
2019-08-06 00:08:03 -0300

    more tests

---
+++ tests/test_functions3.py
@@ -0,0 +1,15 @@
+"""
+Tests all functions used in flow file
+"""
+import unittest
+import config
+from functions3 import assing_collection_pkg_id, write_results_to_ckan
+
+
+class Functions3TestClass(unittest.TestCase):
+
+    def test_assing_collection_pkg_id(self):
+        self.assertIsNone('DO THIS')
+
+    def test_write_results_to_ckan(self):
+        self.assertIsNone('DO THIS')
