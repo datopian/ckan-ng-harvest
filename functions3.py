96e99de59de8501e1c4563ad151a56f9af8aba52
Andres Vazquez <andres@data99.com.ar>
2019-08-06 14:11:14 -0300

    fix merged branches

--- functions3.py
+++ functions3.py
@@ -71,62 +71,8 @@ def write_results_to_ckan(rows):
     c = 0
     for row in rows:
         c += 1
-        if 'is_duplicate' not in row:
-            comparison_results = row['comparison_results']
-            action = comparison_results['action']
-            if action not in actions.keys():
-                actions[action] = {'total': 0, 'success': 0, 'fails': 0}
-            actions[action]['total'] += 1
-
-            dump_comp_res = json.dumps(comparison_results, indent=4)
-            # logger.info(f'Previous results {dump_comp_res}')
-            """ comparison_results is something like this
-            row['comparison_results'] {
-                "action": "update" | "delete" | "create",
-                "ckan_id": "1bfc8520-17b0-46b9-9940-a6646615436c",
-                "new_data": {data json dataset format},
-                "reason": "Some reason for the action"
-                }
-            """
-
-            if action == 'error':
-                results = {'success': False}
-                comparison_results['action_results'] = results
-                yield row
-                continue
-
-            # if it's an update we need to merge internal resources
-            if action == 'update':
-                existing_resources = row['resources']
-            elif action == 'create':
-                existing_resources = None
-
-            if action in ['update', 'create']:
-                datajson_dataset = comparison_results['new_data']
-
-                # add required extras
-
-                schema_version = datajson_dataset['headers']['schema_version']  # 1.1 or 1.0
-                assert schema_version in ['1.0', '1.1']
-                datajson_dataset['source_schema_version'] = schema_version
-                datajson_dataset['source_hash'] = hash_dataset(datasetdict=datajson_dataset)
-
-                # harvest extras
-                # check if a local harvest source is required
-                # https://github.com/ckan/ckanext-harvest/blob/master/ckanext/harvest/logic/action/create.py#L27
-                datajson_dataset['harvest_ng_source_title'] = config.SOURCE_NAME
-                datajson_dataset['harvest_ng_source_id'] = config.SOURCE_ID
-
-                # CKAN hides this extras if we not define as harvest type
-                # if https://github.com/ckan/ckanext-harvest/blob/3a72337f1e619bf9ea3221037ca86615ec22ae2f/ckanext/harvest/plugin.py#L125
-                datajson_dataset['harvest_source_title'] = config.SOURCE_NAME
-                datajson_dataset['harvest_source_id'] = config.SOURCE_ID
-
-                djss = DataJSONSchema1_1(original_dataset=datajson_dataset)
-                # ORG is required!
-                djss.ckan_owner_org_id = config.CKAN_OWNER_ORG
-
-                ckan_dataset = djss.transform_to_ckan_dataset(existing_resources=existing_resources)
+        if 'is_duplicate' in row:
+            continue
 
         comparison_results = row['comparison_results']
         action = comparison_results['action']
@@ -173,14 +119,16 @@ def write_results_to_ckan(rows):
             datajson_dataset['source_schema_version'] = schema_version
             datajson_dataset['source_hash'] = hash_dataset(datasetdict=datajson_dataset)
 
-                if ckan_response['success']:
-                    actions[action]['success'] += 1
-                else:
-                    actions[action]['fails'] += 1
+            # harvest extras
+            # check if a local harvest source is required
+            # https://github.com/ckan/ckanext-harvest/blob/master/ckanext/harvest/logic/action/create.py#L27
+            datajson_dataset['harvest_ng_source_title'] = config.SOURCE_NAME
+            datajson_dataset['harvest_ng_source_id'] = config.SOURCE_ID
 
-            elif action == 'update':
-                cpa = CKANPortalAPI(base_url=config.CKAN_CATALOG_URL,
-                                    api_key=config.CKAN_API_KEY)
+            # CKAN hides this extras if we not define as harvest type
+            # if https://github.com/ckan/ckanext-harvest/blob/3a72337f1e619bf9ea3221037ca86615ec22ae2f/ckanext/harvest/plugin.py#L125
+            datajson_dataset['harvest_source_title'] = config.SOURCE_NAME
+            datajson_dataset['harvest_source_id'] = config.SOURCE_ID
 
             if schema_version == '1.1':
                 djss = DataJSONSchema1_1(original_dataset=datajson_dataset)
@@ -201,30 +149,44 @@ def write_results_to_ckan(rows):
             cpa = CKANPortalAPI(base_url=config.CKAN_CATALOG_URL,
                                 api_key=config.CKAN_API_KEY)
 
-            elif action == 'delete':
-                ckan_id = row['comparison_results']['ckan_id']
-                cpa = CKANPortalAPI(base_url=config.CKAN_CATALOG_URL, api_key=config.CKAN_API_KEY)
+            try:
+                ckan_response = cpa.create_package(ckan_package=ckan_dataset)
+            except Exception as e:
+                ckan_response = {'success': False, 'error': str(e)}
 
-                try:
-                    ckan_response = cpa.delete_package(ckan_package_ir_or_name=ckan_id)
-                except Exception as e:
-                    ckan_response = {'success': False, 'error': str(e)}
+            results = {'success': ckan_response['success']}
+            results['ckan_response'] = ckan_response
 
-                results = {'success': ckan_response['success']}
-                results['ckan_response'] = ckan_response
-
-                if ckan_response['success']:
-                    actions[action]['success'] += 1
-                else:
-                    actions[action]['fails'] += 1
-
-            elif action == 'ignore':
-                continue
-                results = {'success': True}
-
+            if ckan_response['success']:
+                actions[action]['success'] += 1
             else:
-                error = 'Unexpected action for this dataset: {}'.format(action)
-                results = {'success': False, 'error': error}
+                actions[action]['fails'] += 1
+
+        elif action == 'update':
+            cpa = CKANPortalAPI(base_url=config.CKAN_CATALOG_URL,
+                                api_key=config.CKAN_API_KEY)
+
+            try:
+                ckan_response = cpa.update_package(ckan_package=ckan_dataset)
+            except Exception as e:
+                ckan_response = {'success': False, 'error': str(e)}
+
+            results = {'success': ckan_response['success']}
+            results['ckan_response'] = ckan_response
+
+            if ckan_response['success']:
+                actions[action]['success'] += 1
+            else:
+                actions[action]['fails'] += 1
+
+        elif action == 'delete':
+            ckan_id = row['comparison_results']['ckan_id']
+            cpa = CKANPortalAPI(base_url=config.CKAN_CATALOG_URL, api_key=config.CKAN_API_KEY)
+
+            try:
+                ckan_response = cpa.delete_package(ckan_package_ir_or_name=ckan_id)
+            except Exception as e:
+                ckan_response = {'success': False, 'error': str(e)}
 
             results = {'success': ckan_response['success']}
             results['ckan_response'] = ckan_response
@@ -251,6 +213,7 @@ def write_results_to_ckan(rows):
 def write_final_report():
     """ take all generated data and write a final report """
     pass
+
 
 def build_validation_error_email(error_items):
     #header errors
