475272981dcbca06e562518e42dda46e8163b5e0
Andres Vazquez <andres@data99.com.ar>
2019-08-08 19:55:36 -0300

    preparing airflow

--- automate-tasks/airflow/harvest_with_airflow.py
+++ automate-tasks/airflow/harvest_with_airflow.py
@@ -1,3 +1,8 @@
+"""
+Harvester DAGs
+
+"""
+
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
@@ -7,8 +12,8 @@ from libs.data_gov_api import CKANPortalAPI
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2015, 6, 1),
-    'email': ['airflow@example.com'],
+    'start_date': datetime(2020, 1, 1),
+    'email': ['devops@datopian.com'],  # TODO check
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
@@ -16,10 +21,11 @@ default_args = {
     # 'queue': 'bash_queue',
     # 'pool': 'backfill',
     # 'priority_weight': 10,
-    # 'end_date': datetime(2016, 1, 1),
+    # 'end_date': datetime(2020, 2, 1),
 }
 
-dag = DAG('Harvest all at catalog.data.gov', default_args=default_args, schedule_interval=timedelta(days=1))
+dag = DAG('Harvest all at catalog.data.gov', default_args=default_args,
+          schedule_interval=timedelta(weeks=1))
 
 catalog_url = 'http://ckan:5000'
 catalog_api_key = '79744bbe-f27b-46c8-a1e0-8f7264746c86'
@@ -46,12 +52,15 @@ for results in cpa.search_harvest_packages(harvest_type='harvest', source_type='
                 --ckan_api_key {{ ckan_api_key }}
             """
 
-        name = harvest_source['']
+        name = harvest_source['name']
+        # this is the ID of the organization at the external source
+        # we need to get our local organizaion ID
+        ckan_org_id = harvest_source['owner_org']
         params = {
             'name': name,
             'data_json_url': url,
             'harvest_source_id': harvest_source['id'],  # check if this is the rigth ID
-            'ckan_org_id': harvest_source[''],
+            'ckan_org_id': ckan_org_id,
             'catalog_url': catalog_url,
             'ckan_api_key': catalog_api_key
             }
