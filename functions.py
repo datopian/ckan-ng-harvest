751eec449fde70637212cd5331ba37e3895c0d80
Andres Vazquez <andres@data99.com.ar>
2019-08-09 12:03:12 -0300

    fix limit datasets

--- functions.py
+++ functions.py
@@ -62,13 +62,13 @@ def get_data_json_from_url(url):
     datajson.save_validation_errors(path=config.get_datajson_headers_validation_errors_path())
 
     # the real dataset list
-    c = 0
+
+    if config.LIMIT_DATASETS > 0:
+        datajson.datasets = datajson.datasets[:config.LIMIT_DATASETS]
     for dataset in datajson.datasets:
-        c += 1
-        if config.LIMIT_DATASETS == 0 or c <= config.LIMIT_DATASETS:
-            # add headers (previously called catalog_values)
-            dataset['headers'] = datajson.headers
-            yield(dataset)
+        # add headers (previously called catalog_values)
+        dataset['headers'] = datajson.headers
+        yield(dataset)
 
 
 def clean_duplicated_identifiers(rows):
