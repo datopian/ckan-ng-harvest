4c024f30065730c10465f67880e933f36d3227a1
Julie Kramer <thejuliekramer@gmail.com>
2019-08-21 11:20:35 -0400

    look at package for results of harvest

--- tests_using_local_ckan_instance/test_harvest_process.py
+++ tests_using_local_ckan_instance/test_harvest_process.py
@@ -13,6 +13,8 @@ from settings import (HARVEST_SOURCE_ID,
                        CKAN_VALID_USER_ID
                       )
 
+from libs.data_gov_api import CKANPortalAPI
+
 class HarvestTestClass(unittest.TestCase):
 
   def test_harvest(self):
@@ -29,7 +31,16 @@ class HarvestTestClass(unittest.TestCase):
       shell=True,
       stdout=PIPE)
     
-    output,err = result.communicate()
+    result.communicate()
 
     self.assertEqual(result.returncode, 0)
-  
+
+    cpa = CKANPortalAPI(base_url=CKAN_BASE_URL, api_key=CKAN_API_KEY)
+    package_show = cpa.show_package(ckan_package_id_or_name=HARVEST_SOURCE_ID)
+
+    print('the status of the job is {}'.format(package_show['result']['status']['last_job']))
+    
+    self.assertEqual(package_show['success'], True)
+    self.assertEqual(package_show['result']['title'], URL)
+    self.assertEqual(package_show['result']['status']['last_job']['source_id'], HARVEST_SOURCE_ID)
+    self.assertEqual(package_show['result']['organization']['title'], CKAN_ORG_ID)
