d59d4da7e3ede148b75e07648898e35db7fea411
Julie Kramer <thejuliekramer@gmail.com>
2019-08-21 12:53:17 -0400

    create new harvest source after deleting all of them

--- tests_using_local_ckan_instance/test_harvest_process.py
+++ tests_using_local_ckan_instance/test_harvest_process.py
@@ -6,8 +6,7 @@ from subprocess import call, check_output, Popen, PIPE
 
 from logs import logger
 
-from settings import (HARVEST_SOURCE_ID,
-                       CKAN_API_KEY,
+from settings import (CKAN_API_KEY,
                        CKAN_BASE_URL,
                        CKAN_ORG_ID,
                        CKAN_VALID_USER_ID
@@ -15,12 +14,25 @@ from settings import (HARVEST_SOURCE_ID,
 
 from libs.data_gov_api import CKANPortalAPI
 
+
 class HarvestTestClass(unittest.TestCase):
 
   def test_harvest(self):
     NAME = 'rrb'
     URL = 'https://secure.rrb.gov/data.json'
 
+    cpa = CKANPortalAPI(base_url=CKAN_BASE_URL, api_key=CKAN_API_KEY)
+    cpa.delete_all_harvest_sources()
+
+    harvest_source = cpa.create_harvest_source(title=NAME,
+                                                url=URL,
+                                                owner_org_id=CKAN_ORG_ID,
+                                                source_type='datajson',
+                                                notes='Some tests about local harvesting sources creation',
+                                                frequency='WEEKLY')
+                                                
+    HARVEST_SOURCE_ID = harvest_source['result']['id']
+    
     result = Popen(['python3 harvest.py --name {} --url {} --harvest_source_id {} --ckan_owner_org_id {} --catalog_url {} --ckan_api_key {}'.format(
       NAME, 
       URL, 
@@ -35,12 +47,8 @@ class HarvestTestClass(unittest.TestCase):
 
     self.assertEqual(result.returncode, 0)
 
-    cpa = CKANPortalAPI(base_url=CKAN_BASE_URL, api_key=CKAN_API_KEY)
     package_show = cpa.show_package(ckan_package_id_or_name=HARVEST_SOURCE_ID)
-
-    print('the status of the job is {}'.format(package_show['result']['status']['last_job']))
-    
+   
     self.assertEqual(package_show['success'], True)
-    self.assertEqual(package_show['result']['title'], URL)
-    self.assertEqual(package_show['result']['status']['last_job']['source_id'], HARVEST_SOURCE_ID)
+    self.assertEqual(package_show['result']['title'], NAME)
     self.assertEqual(package_show['result']['organization']['title'], CKAN_ORG_ID)
