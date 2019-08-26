d19bca0eaaed304f4f6364a8c4d577e4e79e19f9
Andres Vazquez <andres@data99.com.ar>
2019-08-26 14:39:19 -0300

    startring CSW Urls

--- libs/csw.py
+++ libs/csw.py
@@ -10,8 +10,117 @@ USACE Geospatial CSW: http://metadata.usace.army.mil/geoportal/csw?Request=GetCa
 GeoNode State CSW: http://geonode.state.gov/catalogue/csw?service=CSW&version=2.0.2&request=GetRecords&typenames=csw:Record&elementsetname=brief
 OpenTopography CSW: https://portal.opentopography.org/geoportal/csw
 """
+import requests
+from slugify import slugify
+from urllib.parse import urlparse, urlencode
 
 
 class CSWSource:
     """ A CSW Harvest Source """
-    pass
+    datasets = []  # all datasets included
+    validation_errors = []
+    duplicates = []  # list of datasets with the same identifier
+
+    def __init__(self, url):
+        self.url = url
+
+    def get_original_url(self, harvest_id):
+        # take the URL and add required params
+        parts = urlparse.urlparse(self.url)
+        # urlparse('http://www.cwi.nl:80/%7Eguido/Python.html?q=90&p=881')
+        # ParseResult(scheme='http', netloc='www.cwi.nl:80', path='/%7Eguido/Python.html', params='', query='q=90&p=881', fragment='')
+
+        params = {
+            'SERVICE': 'CSW',
+            'VERSION': '2.0.2',
+            'REQUEST': 'GetRecordById',
+            'OUTPUTSCHEMA': 'http://www.isotc211.org/2005/gmd',
+            'OUTPUTFORMAT': 'application/xml',
+            'ID': harvest_id
+        }
+
+        url = urlparse.urlunparse((
+            parts.scheme,
+            parts.netloc,
+            parts.path,
+            None,
+            urlencode(params),
+            None
+        ))
+
+        return url
+
+    def validate(self):
+        errors = []  # to return list of validation errors
+        # return False, errors
+
+        return True, None
+
+    def remove_duplicated_identifiers(self):
+        unique_identifiers = []
+
+        for dataset in self.datasets:
+            idf = dataset['identifier']
+            if idf not in unique_identifiers:
+                unique_identifiers.append(idf)
+            else:
+                self.duplicates.append(idf)
+                self.datasets.remove(dataset)
+
+        return self.duplicates
+
+    def count_resources(self):
+        """ read all datasets and count resources """
+        total = 0
+        for dataset in self.datasets:
+            pass  # TODO
+        return total
+
+    def save_validation_errors(self, path):
+        dmp = json.dumps(self.validation_errors, indent=2)
+        f = open(path, 'w')
+        f.write(dmp)
+        f.close()
+
+    def save_duplicates(self, path):
+        dmp = json.dumps(self.duplicates, indent=2)
+        f = open(path, 'w')
+        f.write(dmp)
+        f.close()
+
+    def save_datasets_as_data_packages(self, folder_path):
+        """ save each dataset from a data.json source as _datapackage_ """
+        for dataset in self.datasets:
+            package = Package()
+
+            #TODO check this, I'm learning datapackages
+            resource = Resource({'data': dataset})
+            resource.infer()  #adds "name": "inline"
+
+            #FIXME identifier uses incompables characthers as paths (e.g. /).
+            # could exist duplicates paths from different resources
+            # use BASE64 or hashes
+            idf = slugify(dataset['identifier'])
+
+            resource_path = os.path.join(folder_path, f'resource_data_json_{idf}.json')
+            if not resource.valid:
+                raise Exception('Invalid resource')
+
+            resource.save(resource_path)
+
+            package.add_resource(descriptor=resource.descriptor)
+            package_path = os.path.join(folder_path, f'pkg_data_json_{idf}.zip')
+            package.save(target=package_path)
+
+
+class CSWDataset:
+    validation_errors = []
+
+    def validate_dataset(self, item):
+        errors = []
+
+        return errors
+
+    def check_required_field(self):
+
+        return True
