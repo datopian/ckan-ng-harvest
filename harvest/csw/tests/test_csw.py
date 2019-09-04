b188f314c25e4118448ab5111fe29de0a450b371
Andres Vazquez <andres@data99.com.ar>
2019-09-04 10:16:08 -0300

    test 404 CSW

--- harvest/csw/tests/test_csw.py
+++ harvest/csw/tests/test_csw.py
@@ -7,6 +7,7 @@ class TestCSWClass(object):
 
     base_url = 'https://datopian.gitlab.io/ckan-ng-harvest'
 
+    # TODO mock a CSW object
     url_services = [
             # 'http://metadata.arcticlcc.org/csw',
             'http://data.nconemap.com/geoportal/csw?Request=GetCapabilities&Service=CSW&Version=2.0.2',
@@ -15,16 +16,6 @@ class TestCSWClass(object):
             'http://geonode.state.gov/catalogue/csw?service=CSW&version=2.0.2&request=GetRecords&typenames=csw:Record&elementsetname=brief',
             # 'https://portal.opentopography.org/geoportal/csw'
         ]
-
-    """ not in use by now
-    def test_url(self):
-        csw = CSWSource(url='http://cswtest.com/test?p=10')
-        url = csw.get_original_url(harvest_id=99)
-
-        # TODO what about the p=10?
-        new_url = 'http://cswtest.com/test?SERVICE=CSW&VERSION=2.0.2&REQUEST=GetRecordById&OUTPUTSCHEMA=http%3A%2F%2Fwww.isotc211.org%2F2005%2Fgmd&OUTPUTFORMAT=application%2Fxml&ID=99'
-        assert url == new_url
-    """
 
     def test_clean_url(self):
         csw = CSWSource(url='http://data.nconemap.com/geoportal/csw?Request=GetCapabilities&Service=CSW&Version=2.0.2')
