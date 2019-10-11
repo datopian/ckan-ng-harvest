b23665686813bde3eee2ab6c459bf4217d1edcc5
Andres Vazquez <andres@data99.com.ar>
2019-10-11 14:07:03 -0300

    airflow starts today

--- automate-tasks/airflow/dags/harvest_with_airflow.py
+++ automate-tasks/airflow/dags/harvest_with_airflow.py
@@ -19,7 +19,7 @@ source_types = ['datajson', 'csw']
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2019, 8, 8),
+    'start_date': datetime.today(),
     'email': ['devops@datopian.com'],  # TODO check
     'email_on_failure': False,
     'email_on_retry': False,
