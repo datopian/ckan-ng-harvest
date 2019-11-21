36ac0c564f6ba1bd43d8fbb939a255fa823bf3d0
Andres Vazquez <andres@data99.com.ar>
2019-11-21 14:27:26 -0300

    starting master Class

---
+++ harvest/harvester.py
@@ -0,0 +1,5 @@
+class HarvestSource:
+    """ main harvester class to inherit """
+    name = None  # name of the harvest source
+    url = None  # url to harvest from
+    config = None  # configuration (e.g validation_schema)
