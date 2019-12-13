09a6e108f65839d5d5ef4712870882d9c3c7d8e1
Andres Vazquez <andres@data99.com.ar>
2019-12-13 12:33:28 -0300

    upgrade references to external resourcs

--- docker-extras/ckan/docker-entrypoint.d/02-ckanext-datajson.sh
+++ docker-extras/ckan/docker-entrypoint.d/02-ckanext-datajson.sh
@@ -3,6 +3,6 @@
 # echo "Install requirements GSA datajson"
 
 # cd /srv/app/src_extensions/
-# git clone --single-branch --branch 50_upgrade_to_CKAN_28 https://github.com/GSA/ckanext-datajson.git
+# git clone --single-branch --branch datagov https://github.com/GSA/ckanext-datajson.git
 # cd ckanext-datajson
 # pip install -r requirements.txt
